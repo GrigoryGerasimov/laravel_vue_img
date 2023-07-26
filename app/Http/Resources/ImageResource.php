067b22d4964bee11f3ff90ab51cc915fdd59aed4
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-07-26 09:30:56 +0200

    getting post dropzone images for update implemented

--- app/Http/Resources/ImageResource.php
+++ app/Http/Resources/ImageResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class ImageResource extends JsonResource
 {
@@ -15,12 +16,14 @@ class ImageResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'path' => $this->path,
             'url' => $this->url,
             'preview_url' => $this->preview_url,
             'post_id' => $this->post_id,
             'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at
+            'updated_at' => $this->updated_at,
+            'size' => Storage::disk('public')->size('images/' . $this->path)
         ];
     }
 }
